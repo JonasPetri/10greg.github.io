69c9318413c13547ff1e871af8de282290e542ec
Jonas Petri <haren.jonas@gmail.com>
2022-01-14 21:27:34 +0100

    Why did I forget this 😑

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if ("id" in params) {
     var id = Math.floor(Math.random() * length);
 }
 async function loadFile() {
-    var response = await fetch("static/js/quotes.json");
+    var response = await fetch("quotes.json");
     var data = await response.json();
     var text = data[id].text;
     var author = data[id].author;
@@ -16,7 +16,7 @@ async function loadFile() {
 }
 loadFile()
 var namer = String(Math.floor(Math.random() * 5) + 1) + ".svg";
-document.body.style.backgroundImage = "url(static/img/" + namer + ")";
+document.body.style.backgroundImage = "url(" + namer + ")";
 var button = document.getElementById("share")
 button.addEventListener("click", share)
 function share() {
